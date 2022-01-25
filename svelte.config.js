9280c2bb26b8dacfcec8b78629fbec0797263961
elixirevo <elixirevocation@gmail.com>
2022-01-26 04:51:20 +0900

    fix: heroku setting

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import path from 'path'
-// import node from "@sveltejs/adapter-node";
-// import nodePolyfills from 'rollup-plugin-node-polyfills';
+import node from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,12 +14,12 @@ const config = {
 
 	kit: {
 		target: '#svelte',
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		// adapter: node({ env: { port: process.env.PORT } }),
+		// adapter: adapter({
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null
+		// }),
+		adapter: node({ env: { port: process.env.PORT } }),
 		vite: {
 			define: {
 				global: {},
